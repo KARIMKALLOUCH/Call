3a1a7ae0a3c565bd10b2c390fc5795f2398d1ad9
kallouch <karimkallouch47@gmail.com>
2025-04-24 03:18:37 +0100

    add varible in branch test

--- app/src/main/java/com/example/call/MainActivity.java
+++ app/src/main/java/com/example/call/MainActivity.java
@@ -6,10 +6,12 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    String test;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //test
+
     }
 }
